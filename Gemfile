d1ec85e547ec331329e4927c1c09f6a090c7a701
Hironori Ichimiya <hiron@hironytic.com>
2015-01-27 18:39:35 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+# source 'http://production.cf.rubygems.org'
+
+ruby '2.1.5'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'mongo'
+gem 'bson_ext'
---
+++ app.rb
@@ -0,0 +1,38 @@
+# encoding: UTF-8
+
+require 'sinatra'
+require 'sinatra/json'
+require 'mongo'
+
+get '/' do
+  redirect to('/index.html')
+end
+
+def do_with_db
+  mongo_uri = ENV['MONGOLAB_URI']
+  db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
+  client = Mongo::MongoClient.from_uri(mongo_uri)
+  db = client.db(db_name)
+  yield(db)
+end
+
+post '/converter/:api_name' do
+  api_name = params[:api_name]
+  do_with_db do |db|
+    inputs = db.collection('inputs')
+    inputs.find_and_modify({
+      query: {'api_name' => api_name},
+      update: {'params' => params},
+      new: true,
+      upsert: true,
+    })
+    
+    outputs = db.collection('outputs')
+    output = outputs.find_one({'api_name' => api_name})
+    if output == nil then
+      return json({})
+    else
+      return json(output['data'])
+    end
+  end
+end
