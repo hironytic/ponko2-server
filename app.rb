117e50a22b64e8bd5d30f06ff3b93d3d8b4b470a
Hironori Ichimiya <hiron@hironytic.com>
2015-02-11 12:02:38 +0900

    Big changes
    Now supports GET/POST/PUT/DELETE methods and any path.

--- app.rb
+++ app.rb
@@ -4,45 +4,52 @@ require 'sinatra'
 require 'sinatra/json'
 require 'mongo'
 
-def do_with_db
-  mongo_uri = ENV['MONGOLAB_URI']
-  db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
-  client = Mongo::MongoClient.from_uri(mongo_uri)
-  db = client.db(db_name)
-  yield(db)
-end
-
 get '/' do
   redirect to('/index.html')
 end
 
-post '/converter/:api_name' do
-  api_name = params[:api_name]
-  do_with_db do |db|
-    # inputs = db.collection('inputs')
-    # inputs.find_and_modify({
-    #   query: {'api_name' => api_name},
-    #   update: {'api_name' => api_name, 'params' => params},
-    #   new: true,
-    #   upsert: true,
-    # })
+helpers do
+  def process
+    puts "Method: #{request.request_method}"
+    puts "Params: #{request.params}"
     
-    puts "Params: #{params}"
+    mongo_uri = ENV['MONGOLAB_URI']
+    db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
+    client = Mongo::MongoClient.from_uri(mongo_uri)
+    db = client.db(db_name)
+    
+    api_name = params[:captures].first
     
     outputs = db.collection('outputs')
     output = outputs.find_one({'api_name' => api_name})
     if output == nil then
-      next json({})
+      return json({})
     else
       if output['status'] != nil then
         status output['status']
       end
       
       if output['file'] != nil then
-        next send_file output['file']
+        return send_file output['file']
       else
-        next json(output['data'])
+        return json(output['data'])
       end
     end
   end
+end
+
+get %r{(.+)} do
+  process
+end
+
+post %r{(.+)} do
+  process
+end
+
+put %r{(.+)} do
+  process
+end
+
+delete %r{(.+)} do
+  process
 end
